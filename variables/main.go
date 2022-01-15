7999af3284680242ba41994dc8465e57c5ddad1c
Daniel Vinícius <dev.dan.programador@gmail.com>
2022-01-15 14:47:03 -0300

    constantes

--- variables/main.go
+++ variables/main.go
@@ -1,19 +1,23 @@
-package main
+// package main
+package variables
 
-import "fmt"
+// import "fmt"
 
-func main() {
-	a := 10
-	b := "Hello"
-	c := 10.45
-	d := false
-	e := 'W'
-	f := `Uouuu`
+const Xpto int = 10
+const helloWorld string = "Hello World"
 
-	fmt.Printf("%T \n", a)
-	fmt.Printf("%T \n", b)
-	fmt.Printf("%T \n", c)
-	fmt.Printf("%T \n", d)
-	fmt.Printf("%T \n", e)
-	fmt.Printf("%T \n", f)
-}
+// func main() {
+// 	a := 10
+// 	b := "Hello"
+// 	c := 10.45
+// 	d := false
+// 	e := 'W'
+// 	f := `Uouuu`
+
+// 	fmt.Printf("%T \n", a)
+// 	fmt.Printf("%T \n", b)
+// 	fmt.Printf("%T \n", c)
+// 	fmt.Printf("%T \n", d)
+// 	fmt.Printf("%T \n", e)
+// 	fmt.Printf("%T \n", f)
+// }
--- visibility/main.go
+++ visibility/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/Daniel-Vinicius/iniciando_com_golang/scope"
+	"github.com/Daniel-Vinicius/iniciando_com_golang/variables"
 )
 
 func main() {
 	scope.PrintZ()
+	fmt.Println(variables.Xpto)
 }
